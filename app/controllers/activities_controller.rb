abad7e32bdeb297b68c5be1e0114fa2a45706da9
manel boulaabi <nuwr24@gmail.com>
2025-05-20 18:01:21 +0100

    show and index methods

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,15 @@
 class ActivitiesController < ApplicationController
+
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+    # raise
+    @activity = Activity.find(params[:id])
+    # we want to make a new booking on the show page
+    @booking = @activity.user.booking.new
+    @booking.user = current_user
+    redirect_to activities_path
+  end
 end
