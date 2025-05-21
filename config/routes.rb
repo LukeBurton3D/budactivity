c4a5d4beb0ffaf365eaf03699720a3cb13a6e525
Teagan Dorsch <teagandorsch@gmail.com>
2025-05-21 15:28:22 +0100

    added booking form

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :activities do
+    resources :bookings, only: [:new, :create]
+  end
 end
