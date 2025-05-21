39853cc18a80ef76137c3b9df697e009a12ad672
Manel <78561237+manie1313@users.noreply.github.com>
2025-05-21 15:21:50 +0100

    Revert "Activities index"

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,12 +1,3 @@
 class Activity < ApplicationRecord
   belongs_to :user
-  has_many :bookings, dependent: :destroy
-  # activity_categories = ['Sport', 'Cooking Class', 'Theater', 'Skying', 'Food tour']
-  # , inclusion: { in: activity_categories }
-  validates :name, :category, :location, presence: true
-  # :description
-  validates :category, presence: true
-
-  geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/migrate/20250521112640_add_latitude_longitude_to_activities.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeLongitudeToActivities < ActiveRecord::Migration[7.1]
-  def change
-    add_column :activities, :latitude, :float
-    add_column :activities, :longitude, :float
-  end
-end
